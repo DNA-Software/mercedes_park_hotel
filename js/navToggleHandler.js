1091c871958e93482a2c4c886b4b58e7dd7abdd5
Allan Chica <ninjalover249@gmail.com>
2024-12-01 18:32:58 -0300

    update(nav): styles

--- js/navToggleHandler.js
+++ js/navToggleHandler.js
@@ -1,6 +1,12 @@
 const nav = document.querySelector("nav");
 const navToggle = document.querySelector("button");
+const header = document.querySelector("header");
 
 navToggle.addEventListener("click", () => { // Handler para cuando se toca el boton de navegacion
   nav.classList.toggle("open")
+});
+
+window.addEventListener('scroll', () => { // Event listener para el scroll de la pagina
+  if (window.scrollY > 10) header.classList.add('scrolled');
+  else header.classList.remove('scrolled');
 });
